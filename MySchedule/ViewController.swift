7c7b529116085d8f8596beef368bb4809ce4a32f
Radik <RDKJaz@gmail.com>
2022-05-20 18:08:46 +0300

    Initial Commit

---
+++ MySchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySchedule
+//
+//  Created by Radik Gazetdinov on 20.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
